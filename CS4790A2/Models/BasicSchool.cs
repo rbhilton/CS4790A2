b35e47bea46b082e03a7537a96d251fdec3f9e1b
rhilton <rhilton@TE103C-INST>
2017-09-18 10:03:40 -0600

    Add project files.

---
+++ CS4790A2/Models/BasicSchool.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace CS4790A2.Models
+{
+    public class BasicSchool
+    {
+        public static Course getCourse(int? id)
+        {
+            BasicSchoolDbContext db = new BasicSchoolDbContext();
+            Course course = db.courses.Find(id);
+
+            return course;
+        }
+
+        public static List<Course> getAllCourses()
+        {
+            BasicSchoolDbContext db = new BasicSchoolDbContext();
+            return db.courses.ToList();
+        }
+
+    }
+
+    [Table("Course")]
+    public class Course
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Course Number")]
+        public string courseNumber { get; set; }
+        [DisplayName("Course Name")]
+        public string courseName { get; set; }
+        [Range(0,4,ErrorMessage ="What are you thinking???")]
+        [DisplayName("Credit Hours")]
+        public int creditHours { get; set; }
+        [DisplayName("Maximum Enrollment")]
+        public int? maxEnrollment { get; set; }
+    }
+
+    [Table("Section")]
+    public class Section
+    {
+        [Key]
+        public int Id { get; set; }
+        public int sectionID { get; set; }
+        public int sectionNumber { get; set; }
+        public string courseNumber { get; set; }
+        public string sectionDays { get; set; }
+        public DateTime sectionTime { get; set; }
+    }
+
+    public class BasicSchoolDbContext : DbContext
+    {
+        public DbSet<Course> courses { get; set; }
+        public DbSet<Section> sections { get; set; }
+    }
+
+}
---
+++ CS4790A2/Models/Repository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CS4790A2.Models
+{
+    public class Repository
+    {
+        public static Course getCourse(int? id)
+        {
+            Course course = BasicSchool.getCourse(id);
+            return course;
+        }
+    }
+
+    public class CourseAndSections
+    {
+        public Course course { get; set; }
+        public List<Section> sections { get; set; }
+    }
+}
